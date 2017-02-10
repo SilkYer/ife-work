94b80c8631d3ed549fb9d41b542f016f3f4a0cf3
SoloS <420271773@qq.com>
2017-02-10 22:51:59 +0800

    fix some bug
    fix some bug

--- task00002/js/util.js
+++ task00002/js/util.js
@@ -1,58 +1,49 @@
-// task 2.2
+     /**
+ * Created by Gaohaoyang on 2015.4.30
+ */
+// 判断arr是否为一个数组，返回一个bool值
+function isArray(arr) {
+    return typeof arr === "object" && Object.prototype.toString.call(arr) === "[object Array]";
+}
+
+// 判断fn是否为一个函数，返回一个bool值
+function isFunction(fn) {
+    return typeof fn === "function";
+}
+
+//--------------------------------------------------
 // 使用递归来实现一个深度克隆，可以复制一个目标对象，返回一个完整拷贝
 // 被复制的对象类型会被限制为数字、字符串、布尔、日期、数组、Object对象。不会包含函数、正则对象等
-
-
-function cloneObject(src){
-    var a;
+function cloneObject(src) {
+    var o; //result
     if (Object.prototype.toString.call(src) === "[object Array]") {
-        var a = [] ;
+        o = []; //判断是否是数组，并赋初始值
+    } else {
+        o = {};
     }
-    else{
-        var a = {};
-    }
-
-    for(var key in src){
-        if(src.hasOwnProperty(key)){
-            if (typeof src[key] === "object") { //大写Object,则无法识别出对象
-                                                //同 src[key] instanceof Object
-                                                //此时Object为大写
-                                                //详情可以看12.5日笔记
-                a[key] = cloneObject(src[key]);
-
+    for (var i in src) { //遍历这个对象
+        if (src.hasOwnProperty(i)) { //排出继承属性
+            if (typeof src[i] === "object") {
+                o[i] = cloneObject(src[i]); //递归赋值
+            } else {
+                o[i] = src[i]; //直接赋值
             }
-            else{
-                
-                a[key] = src[key];
-            }
-
         }
     }
-
-    return a;
+    return o;
 }
 
+//--------------------------------------------------------------
 // 对数组进行去重操作，只考虑数组中元素为数字或字符串，返回一个去重后的数组
 function uniqArray(arr) {
-    var arrBlank = [];
-
-    //遍历数组
-    for(var i in arr){
-
-    	if(arrBlank.indexOf(arr[i]) == -1){
-
-    		arrBlank.push(arr[i]);
-    	}
+    var newArr = []; //创建空数组
+    for (var i in arr) { //遍历旧数组
+        if (newArr.indexOf(arr[i]) == -1) { //如果新数组中不存在当前元素
+            newArr.push(arr[i]); //新数组中加入当前元素
+        }
     }
-
-    return arrBlank;
+    return newArr;
 }
-
-// 使用示例
-var a = [1, 3, 5, 7, 5, 3];
-var b = uniqArray(a);
-console.log(b); // [1, 3, 5, 7]
-
 
 
 // 中级班同学跳过此题
@@ -60,102 +51,89 @@ console.log(b); // [1, 3, 5, 7]
 // 假定空白字符只有半角空格、Tab
 // 练习通过循环，以及字符串的一些基本方法，分别扫描字符串str头部和尾部是否有连续的空白字符，并且删掉他们，最后返回一个完成去除的字符串
 function simpleTrim(str) {
-    for (var i = 0; i < str.length; i++) {  //索引 = 长度 - 1
-    	if (str.charAt(i) != '' && str.charAt(i) != '\t') {
-    		break;
-    	}
+    var i;
+    var j;
+    for (i = 0; i < str.length; i++) { //从头遍历字符串
+        if (str.charAt(i) != " " && str.charAt(i) != "\t") { //当不为空的时候
+            break; //跳出循环
+        }
     }
-
-    for (var j = str.length - 1 ; j <= 0; j--) {
-    	if (str.charAt(j) != '' && str.charAt(j) != '\t') {
-    		break
-    	}
+    for (j = str.length - 1; j >= 0; j--) {
+        if (str.charAt(j) != " " && str.charAt(j) != "\t") { //当不为空的时候
+            break; //跳出循环
+        }
     }
-
-    return str.slice(i, j-1);
+    return str.slice(i, j + 1); //返回子字符串
 }
 
 // 很多同学肯定对于上面的代码看不下去，接下来，我们真正实现一个trim
 // 对字符串头尾进行空格字符的去除、包括全角半角空格、Tab等，返回一个字符串
 // 尝试使用一行简洁的正则表达式完成该题目
 function trim(str) {
-    // your implement
-    return str.replace(/^\s+|\s+$/g, ''); //不太懂正则表达式
+    if (str.length != -1) {
+        return str.replace(/^\s+|\s+$/g, '');
+        //匹配开头和结尾的空白字符，并全局匹配
+    }
 }
 
-// 使用示例
-var str = '   hi!  ';
-str = trim(str);
-console.log(str); // 'hi!'
-
-
-
-
-
-
-
-
+/*
+ * 除去数组中的空元素
+ */
+function deleteBlank(arr) {
+    var arr2 = [];
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i].match(/\s+/) || arr[i] === "") {
+            continue;
+        } else {
+            arr2.push(arr[i]);
+        }
+    }
+    return arr2;
+}
 
 // 实现一个遍历数组的方法，针对数组中每一个元素执行fn函数，并将数组索引和元素作为参数传递
 function each(arr, fn) {
-   for(var i in arr){
-   	fn(arr[i], i);
-   }
+    for (var i in arr) {
+        fn(arr[i], i);
+    }
 }
 
-// 其中fn函数可以接受两个参数：item和index
 
-// 使用示例
-var arr = ['java', 'c', 'php', 'html'];
-function output(item) {
-    console.log(item)
+
+// 获取一个对象里面第一层元素的数量，返回一个整数
+function getObjectLength(obj) {
+    return Object.keys(obj).length;
 }
-each(arr, output);  // java, c, php, html
-
-// 使用示例
-var arr = ['java', 'c', 'php', 'html'];
-function output(item, index) {
-    console.log(index + ': ' + item)
-}
-each(arr, output);  // 0:java, 1:c, 2:php, 3:html
 
 
 
+//---------------------------------------------------------
 // 判断是否为邮箱地址
-function isEmail(emailStr) {//邮箱:(数字字母,点)的组合,@符号,数字字母,(点,数字字母)
-   var emailReg =  /^(\w+\.)*@\w+(\.\w)*$/;
-   return emailReg.test(emailStr);
-
+function isEmail(emailStr) {
+    var pattern = /^(\w+\.)*\w+@\w+(\.\w+)+$/;
+    return pattern.test(emailStr);
 }
 
 // 判断是否为手机号
 function isMobilePhone(phone) {
-    var phoneReg = /^(\+\d{1,4})?\d{7,11}$/;
-    return phoneReg.test(phone)
+    var pattern = /^(\+\d{1,4})?\d{7,11}$/;
+    return pattern.test(phone);
 }
 
 
 
-
+//-----------------------------------------------------------
 // 为element增加一个样式名为newClassName的新样式
 function addClass(element, newClassName) {
-    var oldClassName = element.className;
-
-    if (oldClassName === " ") {
-    	oldClassName = newClassName;
-    }
-    else{
-    	oldClassName = oldClassName + " " + newClassName;
-    }
-
+    var oldClassName = element.className; //获取旧的样式类
+    element.className = oldClassName === "" ? newClassName : oldClassName + " " + newClassName;
 }
 
 // 移除element中的样式oldClassName
 function removeClass(element, oldClassName) {
-    var originalClass = element.className;
-    var pattern = new RegExp("//b" + oldClassName + "//b");
-
-    return element.className = originalClass.replace(pattern , '');
+    var originClassName = element.className; //获取原先的样式类
+    var pattern = new RegExp("\\b" + oldClassName + "\\b"); //使用构造函数构造动态的正则表达式
+    element.className = trim(originClassName.replace(pattern, ''));
 }
 
 // 判断siblingNode和element是否为同一个父元素下的同一级的元素，返回bool值
@@ -164,117 +142,146 @@ function isSiblingNode(element, siblingNode) {
 }
 
 // 获取element相对于浏览器窗口的位置，返回一个对象{x, y}
-/*
-	因为是相对位置
-
-	所以用getBoundingClientRect()即可
-	如果计算绝对位置
-	才需要scrollLeft/scrollTop
-
-
-*/
 function getPosition(element) {
     var pos = {};
-
-    pos.x = element.getBoundingClientRect().left
-    pos.y = element.getBoundingClientRect().top
-
-    return pos ;
+    pos.x = element.getBoundingClientRect().left + Math.max(document.documentElement.scrollLeft, document.body.scrollLeft);
+    pos.y = element.getBoundingClientRect().top + Math.max(document.documentElement.scrollTop, document.body.scrollTop);
+    return pos;
 }
 
-//接下来挑战一个mini $，它和之前的$是不兼容的，它应该是document.querySelector的功能子集，在不直接使用document.querySelector的情况下，在你的util.js中完成以下任务：
 
+
+//---------------------------------------------------------
 // 实现一个简单的Query
+//多个选择器有点难到我了，看了一些资料觉得思路应该如下：
+//1.如果存在#，直接从#开始向后查
+//2.如果存在tag直接找到所有的tag然后向后查
+//3.样式类，属性，从后向前查，得到它所有的父节点名称，去筛选匹配
+//以上的做法有点太复杂，我还是做一个简单的正向匹配吧。
 function $(selector) {
-    var ele = document;
-    var sele = selector.replace(/\s+/, ' ').split(' ');    // 去除多余的空格并分割
 
-    for (var i = 0, len = sele.length; i < len; i++) {
+    if (!selector) {
+        return null;
+    }
 
-        switch (sele[i][0]) {    // 从子节点中查找
-            case '#':
-                ele = ele.getElementById(sele[i].substring(1));
-                break;
-            case '.':
-                ele = ele.getElementsByClassName(sele[i].substring(1))[0];
-                break;
-            case '[':
-                var valueLoc = sele[i].indexOf('=');
-                var temp = ele.getElementsByTagName('*');
-                var tLen = temp.length;
-                if (valueLoc !== -1) {
-                    var key = sele[i].substring(1, valueLoc);
-                    var value = sele[i].substring(valueLoc + 1, sele[i].length - 1);
-                    for (var j = 0; j < tLen; j++) {
-                        if (temp[j][key] === value) {
-                            ele = temp[j];
-                            break;
-                        }
-                    }
-                }
-                else {
-                    var key = sele[i].substring(1, sele[i].length - 1);
-                    for (var j = 0; j < tLen; j++) {
-                        if (temp[j][key]) {
-                            ele = temp[j];
-                            break;
-                        }
-                    }
-                }
-                break;
-            default :
-                ele = ele.getElementsByTagName(sele[i])[0];
-                break;
+    if (selector == document) {
+        return document;
+    }
+
+    selector = trim(selector);
+    if (selector.indexOf(" ") !== -1) { //若存在空格
+        var selectorArr = selector.split(/\s+/); //拆成数组
+
+        var rootScope = myQuery(selectorArr[0]); //第一次的查找范围
+        var i = null;
+        var j = null;
+        var result = [];
+        //循环选择器中的每一个元素
+        for (i = 1; i < selectorArr.length; i++) {
+            for (j = 0; j < rootScope.length; j++) {
+                result.push(myQuery(selectorArr[i], rootScope[j]));
+            }
+            // rootScope = result;
+            // 目前这个方法还有bug
         }
+        return result[0][0];
+    } else { //只有一个，直接查询
+        return myQuery(selector, document)[0];
     }
-
-    if (!ele) {
-        ele = null;
-    }
-
-    return ele;
 }
 
+/**
+ * 针对一个内容查找结果 success
+ * @param  {String} selector 选择器内容
+ * @param  {Element} root    根节点元素
+ * @return {NodeList数组}    节点列表，可能是多个节点也可能是一个
+ */
+function myQuery(selector, root) {
+    var signal = selector[0]; //
+    var allChildren = null;
+    var content = selector.substr(1);
+    var currAttr = null;
+    var result = [];
+    root = root || document; //若没有给root，赋值document
+    switch (signal) {
+        case "#":
+            result.push(document.getElementById(content));
+            break;
+        case ".":
+            allChildren = root.getElementsByTagName("*");
+            // var pattern0 = new RegExp("\\b" + content + "\\b");
+            for (i = 0; i < allChildren.length; i++) {
+                currAttr = allChildren[i].getAttribute("class");
+                if (currAttr !== null) {
+                    var currAttrsArr = currAttr.split(/\s+/);
+                    // console.log(currAttr);
+                    for (j = 0; j < currAttrsArr.length; j++) {
+                        if (content === currAttrsArr[j]) {
+                            result.push(allChildren[i]);
+                            // console.log(result);
+                        }
+                    }
+                }
+            }
+            break;
+        case "[": //属性选择
+            if (content.search("=") == -1) { //只有属性，没有值
+                allChildren = root.getElementsByTagName("*");
+                for (i = 0; i < allChildren.length; i++) {
+                    if (allChildren[i].getAttribute(selector.slice(1, -1)) !== null) {
+                        result.push(allChildren[i]);
+                    }
+                }
+            } else { //既有属性，又有值
+                allChildren = root.getElementsByTagName("*");
+                var pattern = /\[(\w+)\s*\=\s*(\w+)\]/; //为了分离等号前后的内容
+                var cut = selector.match(pattern); //分离后的结果，为数组
+                var key = cut[1]; //键
+                var value = cut[2]; //值
+                for (i = 0; i < allChildren.length; i++) {
+                    if (allChildren[i].getAttribute(key) == value) {
+                        result.push(allChildren[i]);
+                    }
+                }
+            }
+            break;
+        default: //tag
+            result = root.getElementsByTagName(selector);
+            break;
+    }
+    return result;
+}
 
-
-
-//query做不出来...
-
-
-
-
-//4. 事件
-
-//4.1 任务描述
-
-//我们来继续用封装自己的小jQuery库来实现我们对于JavaScript事件的学习，还是在你的util.js，实现以下函数
-
+/**
+ * ==================事件=======================
+ */
 // 给一个element绑定一个针对event事件的响应，响应函数为listener
 function addEvent(element, event, listener) {
-    if(element.addEventListener){
-        element.addEventListener(event , listener );
-    }
-    else{
-        element.attachEvent(event , listener);
+    if (element.addEventListener) {
+        element.addEventListener(event, listener);
+    } else if (element.attachEvent) {
+        element.attachEvent("on" + event, listener);
     }
 }
 
+// 例如：
 
-
+// addEvent($(".second"), "click", function () {
+//     alert("clicksecond");
+// });
 
 // 移除element对象对于event事件发生时执行listener的响应
 function removeEvent(element, event, listener) {
-    if(element.removeEventListener){
-        element.removeEventListener(event , listener);
-    }
-    else{
-        element.detachEvent(event , listener);
+    if (element.removeEventListenr) {
+        element.removeEventListenr(event, listener);
+    } else if (element.detachEvent) {
+        element.detachEvent("on" + event, listener);
     }
 }
 
 // 实现对click事件的绑定
 function addClickEvent(element, listener) {
-    element.addEventListener('click' , listener); //使用了上面写好的方法 增加兼容性
+    addEvent(element, "click", listener);
 }
 
 // 实现对于按Enter键时的事件绑定
@@ -286,11 +293,7 @@ function addEnterEvent(element, listener) {
     });
 }
 
-/*我们增加了一个按钮，当点击按钮时，改变list里面的项目，这个时候你再点击一下li，绑定事件不再生效了。
-那是不是我们每次改变了DOM结构或者内容后，都需要重新绑定事件呢？当然不会这么笨，接下来学习一下事件代理，
-然后实现下面新的方法：*/
-//事件代理
-//我理解是 element为子元素 tag为父元素 这样的delagation才有意义
+// 事件代理
 function delegateEvent(element,tag,eventName,listener){
     addEvent(element, eventName, function(event){
         var target = event.target || event.srcElement;
@@ -300,106 +303,89 @@ function delegateEvent(element,tag,eventName,listener){
     });
 }
 
-//BOM
-//任务与实现：
+// 估计有同学已经开始吐槽了，函数里面一堆$看着晕啊，那么接下来把我们的事件函数做如下封装改变：
 
-// 设置cookie
+$.on = function(selector, event, listener) {
+    addEvent($(selector), event, listener);
+};
+$.click = function(selector, listener) {
+    addClickEvent($(selector), listener);
+};
+$.un = function(selector, event, listener) {
+    removeEvent($(selector), event, listener);
+};
+$.delegate = function(selector, tag, event, listener) {
+    delegateEvent($(selector), tag, event, listener);
+};
 
-function setCookie(cookieName, cookieValue, expiredays) {
-    //encodeURIComponent可把某些字符转成十六进制
-    //cookie名称遵循键值规则 中间不能有符号 可以用encode等方法转义
-    //cookie内,不同属性之间用分号间隔
-    var cookie = cookieName + "=" + encodeURIComponent(cookieValue) + ";" + expiredays; 
 
-    document.cookie = cookie ;
-}
+// ---------5 BOM-------
 
 // 判断是否为IE浏览器，返回-1或者版本号
-function isIE(){ 
-    var u_agent = navigator.userAgent; 
-    var browser_name= '-1'; 
+function isIE() {
+    var s = navigator.userAgent.toLowerCase();
+    console.log(s);
+    //ie10的信息：
+    //mozilla/5.0 (compatible; msie 10.0; windows nt 6.2; trident/6.0)
+    //ie11的信息：
+    //mozilla/5.0 (windows nt 6.1; trident/7.0; slcc2; .net clr 2.0.50727; .net clr 3.5.30729; .net clr 3.0.30729; media center pc 6.0; .net4.0c; .net4.0e; infopath.2; rv:11.0) like gecko
+    var ie = s.match(/rv:([\d.]+)/) || s.match(/msie ([\d.]+)/);
+    if (ie) {
+        return ie[1];
+    } else {
+        return -1;
+    }
+}
 
-    if(u_agent.indexOf('Trident')>-1&&u_agent.indexOf('rv:11')>-1){ 
-    browser_name='IE11'; 
-    }else if(u_agent.indexOf('MSIE')>-1&&u_agent.indexOf('Trident')>-1){ 
-    browser_name='IE(8-10)'; 
-    }else if(u_agent.indexOf('MSIE')>-1){ 
-    browser_name='IE(6-7)'; 
-    }else{ 
-    browser_name;
-    } 
-
-    document.write('browser_name:'+browser_name+'<br>'); 
-} 
+// 设置cookie
+function setCookie(cookieName, cookieValue, expiredays) {
+    var cookie = cookieName + "=" + encodeURIComponent(cookieValue);
+    if (typeof expiredays === "number") {
+        cookie += ";max-age=" + (expiredays * 60 * 60 * 24);
+    }
+    document.cookie = cookie;
+}
 
 // 获取cookie值
 function getCookie(cookieName) {
-    var s_cookie = document.cookie.split(";"); //先将cookie分开
-
-    var a_cookie = s_cookie.split("="); //把cookie拆掉键值对
-
-    for(var key in a_cookie){
-        if(a_cookie[key] == "cookieName"){
-            return a_cookie[key]        
-        }
+    var cookie = {};
+    var all = document.cookie;
+    if (all === "") {
+        return cookie;
     }
-
+    var list = all.split("; ");
+    for (var i = 0; i < list.length; i++) {
+        var p = list[i].indexOf("=");
+        var name = list[i].substr(0, p);
+        var value = list[i].substr(p + 1);
+        value = decodeURIComponent(value);
+        cookie[name] = value;
+    }
+    return cookie;
 }
 
-//6.Ajax
-
-//6.1 任务描述
-
-//学习Ajax，并尝试自己封装一个Ajax方法。实现如下方法：
-
-//function ajax(url, options) {
-    // your implement
-//}
-
-// 使用示例：
-/*ajax(
-    'http://localhost:8080/server/ajaxtest', 
-    {
-        data: {
-            name: 'simon',
-            password: '123456'
-        },
-        onsuccess: function (responseText, xhr) {
-            console.log(responseText);
-        }
-    }
-);*/
-
-/*
-options是一个对象，里面可以包括的参数为：
-
-type: post或者get，可以有一个默认值
-data: 发送的数据，为一个键值对象或者为一个用&连接的赋值字符串
-onsuccess: 成功时的调用函数
-onfail: 失败时的调用函数
-*/
-
+//-------------Ajax---------------
+// 学习Ajax，并尝试自己封装一个Ajax方法。实现如下方法：
 function ajax(url, options) {
-    //data 是个对象 根据要求处理成键值对 处理完成后使用send()函数发出
 
-    var dataResult;
-    var str = '';
-    for(var key in options.data){
-       str = str + key + '=' + options.data[key] + '&';
+    var dataResult; //结果data
+
+    // 处理data
+    if (typeof(options.data) === 'object') {
+        var str = '';
+        for (var c in options.data) {
+            str = str + c + '=' + options.data[c] + '&';
+        }
+        dataResult = str.substring(0, str.length - 1);
     }
-    dataResult = str.substring(0,str.length - 1);
 
-    //处理type
-    
-    options.type = options.type || 'GET'
+    // 处理type
+    options.type = options.type || 'GET';
 
-    //生成xmlHttpRequest对象
-    //问号表示问号前的语句成立与否
-    //成立执行冒号前语句,不成立执行冒号后语句
+    //获取XMLHttpRequest对象
     var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
 
-    //发送请求
-
+    // 发送请求
     xhr.open(options.type, url);
     if (options.type == 'GET') {
         xhr.send(null);
@@ -407,19 +393,32 @@ function ajax(url, options) {
         xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
         xhr.send(dataResult);
     }
-    
 
-    //任务要求成功后执行onsucess函数,判断成功与否则用到了onreadystatechange函数的两个属性~
-    xhr.onreadystatechange = function(){
-        if(xhr.readyState == 4 && xhr.status == 200){
-            options.onsuccess(xhr.responseText, xhr.responseXML);
+    // readyState
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                if (options.onsuccess) {
+                    options.onsuccess(xhr.responseText, xhr.responseXML);
+                }
+            } else {
+                if (options.onfail) {
+                    options.onfail();
+                }
+            }
         }
-        else if(options.onfail){
-            options.onfail();
-        }
-        
-        
-    }
+    };
 }
 
-
+// 使用示例：
+// ajax(
+//     'http://localhost:8080/server/ajaxtest', {
+//         data: {
+//             name: 'simon',
+//             password: '123456'
+//         },
+//         onsuccess: function(responseText, xhr) {
+//             console.log(responseText);
+//         }
+//     }
+// );　
